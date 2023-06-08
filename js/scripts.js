4159d8d025317cc3338fcd0c01ab96bee22fe7ff
J.J. Hibner <jjmhibner@gmail.com>
2023-06-07 19:11:22 -0700

    added subtraction, multiplication, and division functions and alerts

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,31 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 // user interface logic 
 const number1 = Number(prompt("Enter a number:"));
 const number2 = Number(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+let sum = add(number1, number2);
+let difference = subtract(number1, number2);
+let product = multiply(number1, number2);
+let quotient = divide(number1, number2);
+
+window.alert(`The addition of your numbers ${number1} & ${number2} equals ${sum}.`);
+window.alert(`The subtraction of your numbers ${number1} & ${number2} equals ${difference}.`);
+window.alert(`The multiplication of your numbers ${number1} & ${number2} equals ${product}.`);
+window.alert(`The division of your numbers  ${number1} & ${number2} equals ${quotient}.`);
 
 
 /*window.alert("The sum is " + add(3,5) + ".");
